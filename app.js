c0a909ce111e4fdab52ed8357033b2f4ce2a4365
sharbel93 <sharbel@techfactory.ltd>
2019-11-12 12:48:54 +0300

    serving static files - Node Modules

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')));
+
 app.get('/', function(req, res) {
    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
