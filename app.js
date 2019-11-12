17e0cd133f7dbc143b3b0cdfbd594353b1395676
sharbel93 <sharbel@techfactory.ltd>
2019-11-12 11:50:29 +0300

    debugging options

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('tiny'));
 app.get('/', function(req, res) {
    res.send('Hello from my library app');
 });
 
 app.listen(3000, function(){
-   console.log('listening on port 3000');
+   // console.log(`listening on port ${chalk.green(3000)}`);
+   debug(`listening on port ${chalk.green(3000)}`);
 });
